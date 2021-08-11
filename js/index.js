5335c2f33c0dad34bab8c4991fd2d5dd12fc1930
993675454 <993675454@qq.com>
2021-08-11 14:45:15 +0800

    deploy

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || []
+  (function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?023f68794c2051f4ae7a8f1260948644";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })()
